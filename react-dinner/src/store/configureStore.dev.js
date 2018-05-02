7d8cb0773c11117fa0d358e512c2af0092d34e6c
chenfangfang <chenfangfang@xiaoyouzi.com>
2018-05-02 10:07:08 +0800

    开启 redux-devtools

--- react-dinner/src/store/configureStore.dev.js
+++ react-dinner/src/store/configureStore.dev.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 import reducers from '../reducers';
@@ -6,11 +6,15 @@ import reducers from '../reducers';
 const sagaMiddleware = createSagaMiddleware();
 const logger = createLogger();
 
+// 开启redux-devtools
+const reduxToolExtension = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = reduxToolExtension || compose;
+
 export default function configureStore(initialState) {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(sagaMiddleware)
+    composeEnhancers(applyMiddleware(sagaMiddleware, logger))
   );
   store.run = sagaMiddleware.run;
 
